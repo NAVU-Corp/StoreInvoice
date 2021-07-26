04f872cad52fd0b2dec7c8ebd4cfd3cebe05f003
nam.phan <nam.phan@yoot.vn>
2021-07-26 20:16:18 +0700

    Init Project

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  target: "electron-renderer",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    esmodules: true,
+                  },
+                },
+              ],
+              "@babel/preset-react",
+            ],
+          },
+        },
+      },
+      {
+        test: [/\.s[ac]ss$/i, /\.css$/i],
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "bundle"),
+  },
+};
