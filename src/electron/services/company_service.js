21c69efb623f5d2a6b6d62bf61d491fbc1013e22
nam.phan <nam.phan@yoot.vn>
2021-08-24 00:38:32 +0700

    Analyze PDF

--- src/electron/services/company_service.js
+++ src/electron/services/company_service.js
@@ -8,14 +8,28 @@ class CompanyService {
   async createCompany(company, { 
     dateNow,
   }) {
-    this.checkValidInput(company);
+    let message = await this.checkValidInput(company);
+    if(message) {
+      throw({
+        result: 0,
+        message: message,
+      });
+    }
+    
     return this.companyRepository.create(company, { dateNow });
   }
 
-  updateCompany(company, { 
+  async updateCompany(company, { 
     dateNow,
   }) {
-    this.checkValidInput(company);
+    let message = this.checkValidInput(company);
+    if(message) {
+      throw({
+        result: 0,
+        message: message,
+      });
+    }
+
     return this.companyRepository.update(company, { dateNow });
   }
 
@@ -33,22 +47,18 @@ class CompanyService {
     return this.companyRepository.getList(filter);
   }
 
-  checkValidInput(company) {
-    let resultTaxCodeChacker = this.companyRepository.checkExitsTaxCode(company.taxcode, company.id || 0);
+  async checkValidInput(company) {
+    let resultTaxCodeChacker = await this.companyRepository.checkExitsTaxCode(company.taxcode, company.id || 0);
     if(resultTaxCodeChacker && resultTaxCodeChacker.numExists && resultTaxCodeChacker.numExists > 0) {
-      throw({
-        result: 0,
-        message: `Mã số thuế này đã được sử dụng.`
-      });
+      return `Mã số thuế này đã được sử dụng.`;
     }
 
-    let resultNameChecker = this.companyRepository.checkExitsName(company.name, company.id || 0);
+    let resultNameChecker = await this.companyRepository.checkExitsName(company.name, company.id || 0);
     if(resultNameChecker && resultNameChecker.numExists && resultNameChecker.numExists > 0) {
-      throw({
-        result: 0,
-        message: `Tên công ty này đã được sử dụng.`
-      });
+      return `Tên công ty này đã được sử dụng.`;
     }
+
+    return '';
   }
 }
 
